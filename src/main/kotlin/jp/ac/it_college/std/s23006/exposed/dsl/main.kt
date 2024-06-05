22a2b776faf8d95706e4376438549afe767f971b
itc-s23006 <s23006@std.it-college.ac.jp>
2024-06-05 09:48:44 +0900

    ServerSide Kotlin Chapter5 Finish
    Completion
    
    6/4 Finish

--- src/main/kotlin/jp/ac/it_college/std/s23006/exposed/dsl/main.kt
+++ src/main/kotlin/jp/ac/it_college/std/s23006/exposed/dsl/main.kt
@@ -1,16 +1,13 @@
 package jp.ac.it_college.std.s23006.exposed.dsl
 
 import org.jetbrains.exposed.dao.id.IntIdTable
-import org.jetbrains.exposed.sql.Database
+import org.jetbrains.exposed.sql.*
 import org.jetbrains.exposed.sql.SqlExpressionBuilder.eq
-import org.jetbrains.exposed.sql.StdOutSqlLogger
-import org.jetbrains.exposed.sql.addLogger
-import org.jetbrains.exposed.sql.selectAll
 import org.jetbrains.exposed.sql.transactions.transaction
 
 fun main() {
     Database.connect(
-        url = "jdbc:mariadb://127.0.01:3306/example",
+        url = "jdbc:mariadb://127.0.0.1:3306/example",
         driver = "org.mariadb.jdbc.Driver",
         user = "root",
         password = "maria"
@@ -21,13 +18,82 @@ fun main() {
         println("----5.4.3----")
         val user = UserTable.selectAll().where {
             UserTable.id eq 101
-        }.single()
+        }.single().let(::User)
         println(user)
+
+        println("----5.4.4----")
+        val userList = UserTable.selectAll().where {
+            UserTable.name eq "Jiro"
+        }.map(::User)
+        println(userList)
+
+        println("----5.4.6----")
+        val userList2 = UserTable.selectAll().where {
+            UserTable.age greaterEq 25
+        }.map(::User)
+        println(userList2)
+
+        println("----5.4.8----")
+        val count = UserTable.selectAll().where {
+            UserTable.age greaterEq 25
+        }.count()
+        println("条件に一致したデータは　${count}行　です。")
+
+       /* println("----5.4.10----")
+        val statement = UserTable.insert {
+            it[id] = 104
+            it[name] = "Shiro"
+            it[age] = 18
+            it[profile] = "Hello"
+        }
+        println("${statement.insertedCount}行のレコードを挿入しました")*/
+
+/*        println("----5.4.12----")
+        val newUsers = listOf(
+            User(105, "Goro", 15, "Hello"),
+            User(106, "Rokurou", 13, "Hello")
+        )
+        val statement = UserTable.batchInsert(newUsers) { (id, name, age, profile) ->
+            this[UserTable.id] = id
+            this[UserTable.name] = name
+            this[UserTable.age] = age
+            this[UserTable.profile] = profile
+        }
+        println("${statement.count()}行のレコードを挿入しました")*/
+
+        println("----5.4.14----")
+        val updateCount = UserTable.update({
+            UserTable.id eq 105
+        }) {
+            it[profile] = "Bye"
+        }
+        println("${updateCount}行のデータを更新しました")
+
+        println("----5.4.20----")
+        val deleteCount = UserTable.deleteWhere {
+            UserTable.id eq 102
+        }
+        println("${deleteCount}行のデータを削除しました")
     }
+
 }
 
 object UserTable : IntIdTable("user") {
     val name = varchar("name", 16)
     val age = integer("age")
     val profile = varchar("profile", 64)
+}
+
+data class User(
+    val id: Int,
+    val name: String,
+    val age: Int,
+    val profile: String
+) {
+    constructor(row: ResultRow) : this(
+        id = row[UserTable.id].value,
+        name = row[UserTable.name],
+        age = row[UserTable.age],
+        profile = row[UserTable.profile]
+    )
 }
